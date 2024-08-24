3f2f6628399a655953d20ec2fe6c0ff8575cc17c
hungtd412 <tdhung41204@gmail.com>
2024-08-24 22:56:57 +0700

    basic form submission

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
 
 Route::get('/terms', function () {
     return view('terms');
 });
 
-Route::get('/profile', [ProfileController::class, 'index']);
 Route::get('/x', function () {
     return view('x');
 });
