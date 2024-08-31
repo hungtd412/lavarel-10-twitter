9192b142dedacdde0bd8467aae207390e3837b50
hungtd412 <tdhung41204@gmail.com>
2024-08-31 21:37:15 +0700

    resource routing

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,32 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
+
+
+// Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function () {
+
+//     Route::get('{idea}', [IdeaController::class, 'show'])->name('show');
+
+// Route::group(['middleware' => ['auth']], function () {
+//         Route::post('', [IdeaController::class, 'store'])->name('store');
+
+//         Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+
+//         Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
+
+//         Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+//         //middleware('auth'): if the user did not log in, the middleware will direct them to login page
+
+// Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+//     });
+// });
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function () {
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
 
-    Route::post('', [IdeaController::class, 'store'])->name('store');
+Route::resource('ideas', IdeaController::class)->only('show');
 
-    Route::get('{idea}', [IdeaController::class, 'show'])->name('show');
-
-    Route::group(['middleware' => ['auth']], function () {
-
-        Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-
-        Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
-
-        Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-        //middleware('auth'): if the user did not log in, the middleware will direct them to login page
-
-        Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
-
+Route::resource('ideas.comments', CommentController::class)->only('store')->middleware('auth');
 
 Route::get('/terms', function () {
     return view('terms');
