f6443af9d80d1a70715df5c5ecbe38ff50aff532
hungtd412 <tdhung41204@gmail.com>
2024-09-21 11:02:42 +0700

    use cache to load top users from cache, because top users list does not change frequently

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Models\Idea;
 use App\Models\User;
 use App\Policies\IdeaPermissions;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\App;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -28,11 +30,21 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        Debugbar::enable();
+
+        //we should not cache contents changing frequently
+        //php artisan cache:clear = Cache::flush()
+        //rememberForever does not need ttl
+        //Cache::forget('topUsers')
+        $topUsers = Cache::remember('topUsers', Carbon::now()->addMinutes(5)->addSeconds(3), function () {
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'desc')
+                ->limit(5)->get();
+        });
+
         View::share(
             "topUsers",
-            User::withCount('ideas')
-                ->orderBy('ideas_count', 'desc')
-                ->limit(5)->get()
+            $topUsers
         );
 
 
